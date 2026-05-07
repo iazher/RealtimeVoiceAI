476a7e0bc2920c988ee6eca9d9753ac65cbffac7
iazher <imanazher00@gmail.com>
2026-05-08 00:09:06 +0500

    Initial Commit

---
+++ RealtimeVoiceAI/RealtimeVoiceAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealtimeVoiceAIApp.swift
+//  RealtimeVoiceAI
+//
+//  Created by Iman Azher on 08/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct RealtimeVoiceAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
